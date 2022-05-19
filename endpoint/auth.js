cf1b54974ad2260dd448b7797509da4eb78abc32
bydzen <svnteentthones@gmail.com>
2022-05-20 00:37:58 +0700

    change the callback URL 🔙

--- endpoint/auth.js
+++ endpoint/auth.js
@@ -10,7 +10,7 @@ passport.use(
         {
             clientID: GOOGLE_CLIENT_ID,
             clientSecret: GOOGLE_CLIENT_SECRET,
-            callbackURL: 'http://localhost:3000/auth/google/callback',
+            callbackURL: 'https://bydzen.uc.r.appspot.com/auth/google/callback',
             passReqToCallback: true,
         },
         function (request, accessToken, refreshToken, profile, done) {
