8c6b700cb82d7fc557180c473837be80e8118313
bydzen <svnteentthones@gmail.com>
2022-05-21 13:11:12 +0700

    fix all the auth 👤

--- endpoint/server.js
+++ endpoint/server.js
@@ -1,21 +1,47 @@
+// Require module
 var express = require('express'),
     http = require('http'),
     bcrypt = require('bcrypt'),
     session = require('express-session'),
-    passport = require('passport');
+    passport = require('passport'),
+    bodyParser = require('body-parser');
 
-require('./database');
-var connection = require('./database');
-
+// Google Auth required
 require('./auth');
 
+// MySQL required
+var connection = require('./database');
+
+// The app express
 var app = express();
 
+// Middleware
+// Express session
+app.use(
+    session({
+        secret: 'rebage',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+// Parsing the body
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+// Using passport
+app.use(passport.initialize());
+app.use(passport.session());
+// Parsing the JSON
 app.use(express.json());
+app.use(bodyParser.json());
+// App port
+app.set('port', process.env.PORT || 3000);
+// Trust proxy
+app.set('trust proxy', true);
+// View engine ejs
+app.set('view engine', 'ejs');
 
+// Connect and check the database
 connection.connect();
-
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
     console.log(
@@ -25,31 +51,25 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     );
 });
 
-app.set('port', process.env.PORT || 3000);
-
+// Root path
 app.get('/', function (req, res) {
-    res.send(
-        '<h2>Base Endpoint</h2><p>🤖 <a href="/authentication/google">Authenticate with Google</a> 🤖<br><br><mark>/</mark> for this page<br><mark>/authentication/signup</mark> for signup with params<br><mark>/logout</mark> for logging out google auth<br><mark>/deleteall</mark> for delete all users<br><mark>/alluser</mark> for retrieving all users listed</p>'
-    );
+    res.render('index');
 });
 
-// Google Auth
-function isLoggedIn(req, res, next) {
-    req.user ? next() : res.sendStatus(401);
+// Google Authentication
+// Check if the user is logged in
+function isAuthWithGoogle(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
 }
-app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 
-app.get('/authentication/google', function (req, res) {
-    res.send('<a href="/auth/google">Login with Google</a>');
-});
-
+// Google login
 app.get(
     '/auth/google',
     passport.authenticate('google', { scope: ['email', 'profile'] })
 );
 
+// Google callback
 app.get(
     '/auth/google/callback',
     passport.authenticate('google', {
@@ -58,64 +78,144 @@ app.get(
     })
 );
 
-app.get('/protected', isLoggedIn, (req, res) => {
-    res.send(`Hello ${req.user.displayName}<br><a href="/logout">Logout</a>`);
+// Google protected
+app.get('/protected', isAuthWithGoogle, (req, res) => {
+    res.send(
+        `<p style="font-family: monospace">Welcome to the protected page, ${req.user.name}!<br />
+        your email is: ${req.user.email}<br />
+        your id is: ${req.user.id}<br />
+        your avatar is: <br />
+        <img src="${req.user.avatar}" /><br />
+        <a href="/signout">Signout</a></p>`
+    );
 });
 
-app.get('/logout', (req, res) => {
-    req.logout();
-    req.session.destroy();
-    res.send('Good Bye! 👋 <br><a href="/">Go to Homepage</a>');
-});
-
+// Google failure
 app.get('/auth/google/failure', (req, res) => {
-    res.send('Failed to authenticate..');
+    res.send(
+        '<p style="font-family: monospace">Failed to authenticate <br><a href="/">Go to Homepage</a></p>'
+    );
 });
 
-// Signup API endpoint for user registration
-app.post('/authentication/signup', async (req, res, next) => {
+// Regular Authentication
+// Check if the user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.session.user) return next();
+    res.redirect('/login');
+}
+
+// Regular registration
+app.get('/register', function (req, res) {
+    // Check if the user is logged in
+    if (req.session.user) return res.redirect('/dashboard');
+    res.render('register');
+});
+
+app.post('/register', function (req, res) {
+    // Body request
     var name = req.body.name;
     var email = req.body.email;
     var password = req.body.password;
 
-    const salt = await bcrypt.genSalt(10);
-    const encryptedPassword = await bcrypt.hash(password, salt);
-
-    var query = 'INSERT INTO user (name, email, password) VALUES (?, ?, ?)';
-    connection.query(query, [name, email, encryptedPassword], (err, result) => {
-        if (err) {
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.status(200).send('User registered successfully');
+    // Check if the user already exists
+    connection.query(
+        'SELECT * FROM user WHERE email = ?',
+        [email],
+        function (error, results, fields) {
+            if (error) throw error;
+            if (results.length > 0) {
+                res.send(
+                    '<p style="font-family: monospace">User already exists</p>'
+                );
+            } else {
+                // Hash the password
+                bcrypt.hash(password, 10, function (err, hash) {
+                    // Insert the user
+                    connection.query(
+                        'INSERT INTO user (name, email, password) VALUES (?, ?, ?)',
+                        [name, email, hash],
+                        function (error, results, fields) {
+                            if (error) throw error;
+                            res.send(
+                                `<p style="font-family: monospace">User created successfully</p><br />
+                                <a href="/">Go to Homepage</a><br />
+                                <a href="/login">Login</a>`
+                            );
+                        }
+                    );
+                });
+            }
         }
-    });
+    );
 });
 
-// Login API endpoint for user login and compare password
-app.post('/authentication/login', async (req, res, next) => {
+// Regular login
+app.get('/login', function (req, res) {
+    // Check if the user is logged in
+    if (req.session.user) return res.redirect('/dashboard');
+    res.render('login');
+});
+
+app.post('/login', function (req, res) {
+    // Body request
     var email = req.body.email;
     var password = req.body.password;
 
-    var query = 'SELECT * FROM user WHERE email = ?';
-    connection.query(query, [email], (err, result) => {
-        if (err) {
-            res.status(500).send('Internal Server Error');
-        } else {
-            if (result.length > 0) {
-                const isPasswordValid = require('bcrypt').compareSync(
+    // Check if the user exists
+    connection.query(
+        'SELECT * FROM user WHERE email = ?',
+        [email],
+        function (error, results, fields) {
+            if (error) throw error;
+            if (results.length > 0) {
+                // Check if the password is correct
+                bcrypt.compare(
                     password,
-                    result[0].password
+                    results[0].password,
+                    function (err, isMatch) {
+                        if (isMatch) {
+                            // Create the session
+                            req.session.user = results[0];
+                            res.redirect('/dashboard');
+                        } else {
+                            res.send(
+                                '<p style="font-family: monospace">Wrong password</p>'
+                            );
+                        }
+                    }
                 );
-                if (isPasswordValid) {
-                    res.status(200).send('Login Successful');
-                } else {
-                    res.status(401).send('Invalid Password');
-                }
             } else {
-                res.status(401).send('Invalid Email');
+                res.send(
+                    '<p style="font-family: monospace">User does not exist</p>'
+                );
             }
         }
-    });
+    );
+});
+
+// Protected dashboard
+app.get('/dashboard', isLoggedIn, (req, res) => {
+    // check if the user is logged in
+    if (req.session.user) {
+        res.send(
+            `<p style="font-family: monospace">Welcome to the dashboard, ${req.session.user.name}!<br />
+            <a href="/signout">Signout</a><br />
+            <a href="/">Go to Homepage as Loggedin</a></p>`
+        );
+    } else {
+        res.send(
+            `<p style="font-family: monospace">You are not logged in,<br />
+            <a href="/login">Login</a></p>`
+        );
+    }
+});
+
+// Regular signout
+app.get('/signout', function (req, res) {
+    req.session.destroy();
+    res.send(
+        `<p style="font-family: monospace">Good Bye! <br><a href="/">Go to Homepage</a></p>`
+    );
 });
 
 // Other routes to check if the server is working properly
@@ -123,24 +223,38 @@ app.post('/authentication/login', async (req, res, next) => {
 app.get('/alluser', function (req, res) {
     connection.query('SELECT * FROM user', function (err, rows) {
         if (err) {
-            res.status(500).send('Internal Server Error');
+            res.status(500).send(
+                '<p style="font-family: monospace">Internal Server Error</p>'
+            );
         } else {
             res.status(200).send(rows);
         }
     });
 });
 
-// delete all user
-app.delete('/deleteall', function (req, res) {
-    connection.query('DELETE FROM user', function (err, result) {
+// delete all user get
+app.get('/deletealluser', function (req, res) {
+    res.send(
+        '<p style="font-family: monospace">Are you sure you want to delete all users?<br />' +
+            '<a href="/deletealluser/yes">Yes</a> | <a href="/">No</a></p>'
+    );
+});
+
+app.get('/deletealluser/yes', function (req, res) {
+    connection.query('DELETE FROM user', function (err, rows) {
         if (err) {
-            res.status(500).send('Internal Server Error');
+            res.status(500).send(
+                '<p style="font-family: monospace">Internal Server Error</p>'
+            );
         } else {
-            res.status(200).send('All user deleted successfully');
+            res.status(200).send(
+                '<p style="font-family: monospace">All user deleted</p>'
+            );
         }
     });
 });
 
+// Creating the server
 http.createServer(app).listen(app.get('port'), function () {
-    console.log('Server: http://localhost:' + app.get('port'));
+    console.log('Listening to ' + app.get('port'));
 });
