d9ffea9d6af2858ddd191dd17845737d1f9aa1ba
bydzen <svnteentthones@gmail.com>
2022-05-21 13:09:16 +0700

    add info google strategy function 🎖️

--- endpoint/auth.js
+++ endpoint/auth.js
@@ -1,27 +1,52 @@
+// Require passport
 const passport = require('passport');
+// Require GoogleStrategy
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+// Require googleapis
+const { google } = require('googleapis');
 
-const GOOGLE_CLIENT_ID = '<client-id>';
-const GOOGLE_CLIENT_SECRET = '<client-secret>';
+// Credentials
+const GOOGLE_CLIENT_ID = '***';
+const GOOGLE_CLIENT_SECRET = '***';
 
+// Use the GoogleStrategy within Passport
 passport.use(
     new GoogleStrategy(
         {
             clientID: GOOGLE_CLIENT_ID,
             clientSecret: GOOGLE_CLIENT_SECRET,
-            callbackURL: 'https://bydzen.uc.r.appspot.com/auth/google/callback',
+            callbackURL: 'http://***/auth/google/callback',
             passReqToCallback: true,
         },
         function (request, accessToken, refreshToken, profile, done) {
-            return done(null, profile);
+            // Check if the user is already logged in
+            if (request.user) {
+                return done(null);
+            }
+
+            // Create the user
+            const user = {
+                id: profile.id,
+                name: profile.displayName,
+                email: profile.emails[0].value,
+                avatar: profile.photos[0].value,
+            };
+
+            // Add the user to the request
+            request.user = user;
+
+            // Call the next middleware
+            return done(null, user);
         }
     )
 );
 
+// Serialize the user
 passport.serializeUser(function (user, done) {
     done(null, user);
 });
 
+// Deserialize the user
 passport.deserializeUser(function (user, done) {
     done(null, user);
 });
