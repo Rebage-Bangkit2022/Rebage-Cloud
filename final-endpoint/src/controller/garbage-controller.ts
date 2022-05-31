aa002749215b7c928600d1bd34b8b028e09d7033
bydzen <svnteentthones@gmail.com>
2022-05-31 14:59:15 +0700

    Controller for garbages

---
+++ final-endpoint/src/controller/garbage-controller.ts
@@ -0,0 +1,43 @@
+import express, { Router, Request, Response } from 'express';
+import GeneralError from '../model/error';
+import GarbageService from '../service/garbage-service';
+
+class GarbageController {
+    garbageService: GarbageService;
+    router: Router;
+
+    constructor(garbageService: GarbageService, router?: Router) {
+        this.garbageService = garbageService;
+        const r = router ?? express.Router();
+        this.router = r;
+
+        r.post('/api/garbage', this.create);
+        r.get('/api/garbages', this.fetch);
+    }
+
+    create = async (req: Request<{}, { name: string; price: number; image: string }>, res: Response) => {
+        try {
+            const garbage = await this.garbageService.create(req.body);
+            res.json({
+                success: true,
+                data: garbage,
+            });
+        } catch (error) {
+            GeneralError.handle(error, res);
+        }
+    };
+
+    fetch = async (_req: Request, res: Response) => {
+        try {
+            const garbages = await this.garbageService.fetch()
+             res.json({
+                success: true,
+                data: garbages,
+            });
+        } catch (error) {
+             GeneralError.handle(error, res);
+        }
+    };
+}
+
+export default GarbageController;
