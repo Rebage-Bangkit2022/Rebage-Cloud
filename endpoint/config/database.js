b1184c413674b152cb878343c33c0d3cee41c2ef
bydzen <svnteentthones@gmail.com>
2022-05-31 14:53:03 +0700

    Migrating hardcode database info to dotenv

--- endpoint/config/database.js
+++ endpoint/config/database.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 
 // Create a connection
 var connection = mysql.createConnection({
-    host: '***',
-    user: '***',
-    password: '***',
-    database: '***',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 // Export the connection
