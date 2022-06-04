3778a077e77afbd53190ba4ab9950ae598e84e3c
bagus2x <dutapanggulu@gmail.com>
2022-06-04 08:36:07 +0700

    Remove unused import

--- final-endpoint/src/entity/user.ts
+++ final-endpoint/src/entity/user.ts
@@ -1,33 +1,30 @@
-import {
-    Column,
-    Entity,
-    JoinTable,
-    ManyToMany,
-    OneToMany,
-    PrimaryGeneratedColumn,
-} from 'typeorm';
-import Article from './article';
-import TimestampEntity from './entity-timestamp';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import Article from "./article";
+import Detection from "./detection";
+import TimestampEntity from "./entity-timestamp";
 
-@Entity('app_user')
+@Entity("app_user")
 class User extends TimestampEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  name: string;
 
-    @Column({ unique: true })
-    email: string;
+  @Column({ unique: true })
+  email: string;
 
-    @Column({ nullable: true })
-    password: string;
+  @Column({ nullable: true })
+  password: string;
 
-    @Column({ type: 'varchar', nullable: true, default: null })
-    photo!: string | null;
+  @Column({ type: "varchar", nullable: true, default: null })
+  photo!: string | null;
 
-    @OneToMany(() => Article, (article) => article.id)
-    likedArticles: Article[];
+  @OneToMany(() => Article, (article) => article.id)
+  likedArticles: Article[];
+
+  @OneToMany(() => Detection, (detection) => detection.user)
+  detections: Detection[];
 }
 
 export default User;
