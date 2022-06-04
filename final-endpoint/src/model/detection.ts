f571153dbb94c2b4bf6aba947c438f0b555881a8
bagus2x <dutapanggulu@gmail.com>
2022-06-04 08:44:21 +0700

    Create detection model

---
+++ final-endpoint/src/model/detection.ts
@@ -0,0 +1,14 @@
+export interface GetDetectionResponse {
+  id: number;
+  image: string;
+  label: string;
+  boundingBoxes: number[][];
+  scores: number[];
+  total: number;
+  createdAt: Date;
+}
+
+export interface UpdateDetectionRequest {
+  id: number,
+  total: number
+}
