5c40ffdfe5ed52a044bfd5a412879d2922b6ec46
bagus2x <dutapanggulu@gmail.com>
2022-06-04 08:36:42 +0700

    Create entity for detection

---
+++ final-endpoint/src/entity/detection.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import TimestampEntity from "./entity-timestamp";
+import User from "./user";
+
+@Entity({ name: "detection" })
+class Detection extends TimestampEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  image: string;
+
+  @Column()
+  label: string;
+
+  @Column({ name: "bounding_boxes", type: "json" })
+  boundingBoxes: number[][];
+
+  @Column({type: 'json'})
+  scores: number[];
+
+  @Column()
+  total: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @Column({name: 'user_id'})
+  userId: number
+}
+
+export default Detection;
