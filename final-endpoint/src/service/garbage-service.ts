928d3898b6c642affc063ab8a8be3860aae1534d
bydzen <svnteentthones@gmail.com>
2022-06-04 10:12:11 +0700

    Create service for fetch garbage by id and by name

--- final-endpoint/src/service/garbage-service.ts
+++ final-endpoint/src/service/garbage-service.ts
@@ -18,6 +18,20 @@ class GarbageService {
         const garbages = await this.garbageRepository.find();
         return garbages;
     };
+
+    fetchOneId = async (garbageId: number) => {
+        const garbage = await this.garbageRepository.findOne({
+            where: { id: garbageId },
+        });
+        return garbage;
+    };
+
+    fetchOneName = async (garbageName: string) => {
+        const garbage = await this.garbageRepository.findOne({
+            where: { name: garbageName },
+        });
+        return garbage;
+    };
 }
 
 export default GarbageService;
