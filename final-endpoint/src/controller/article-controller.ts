f5cbbfc7b0b96248d02bc760e58422301d3a0d97
bydzen <svnteentthones@gmail.com>
2022-06-08 21:30:36 +0700

    Endpoint fetch and get by id from liked article

--- final-endpoint/src/controller/article-controller.ts
+++ final-endpoint/src/controller/article-controller.ts
@@ -9,6 +9,7 @@ import {
     FetchArticlesRequest,
     GetArticleRequest,
     GetArticlesResponse,
+    GetLikedRequest,
 } from '../model/article';
 import GeneralError from '../model/error';
 import { auth } from './middleware';
@@ -26,6 +27,8 @@ class ArticleController {
         r.get('/api/articles', this.fetch);
         r.get('/api/article/:articleId', this.getArticle);
 
+        r.get('/api/articles/likes', this.fetchLiked);
+        r.get('/api/article/like/:likeId', this.getLiked);
         r.post('/api/article/like', auth, this.like);
         r.delete('/api/article/unlike', auth, this.unlike);
     }
@@ -58,6 +61,18 @@ class ArticleController {
         }
     };
 
+    fetchLiked = async (_req: Request, res: Response) => {
+        try {
+            const likedArticles = await this.articleService.fetchLiked();
+            res.json({
+                success: true,
+                data: likedArticles,
+            });
+        } catch (error) {
+            GeneralError.handle(error, res);
+        }
+    };
+
     getArticle = async (req: Request<GetArticleRequest>, res: Response) => {
         const articleId = parseInt(req.params.articleId);
         try {
@@ -65,6 +80,19 @@ class ArticleController {
             res.status(200).json({
                 success: true,
                 data: article,
+            });
+        } catch (error) {
+            GeneralError.handle(error, res);
+        }
+    };
+
+    getLiked = async (req: Request<GetLikedRequest>, res: Response) => {
+        const likeId = parseInt(req.params.likeId);
+        try {
+            const liked = await this.articleService.getLiked(likeId);
+            res.status(200).json({
+                success: true,
+                data: liked,
             });
         } catch (error) {
             GeneralError.handle(error, res);
